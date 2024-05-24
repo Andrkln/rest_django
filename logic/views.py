76471f5a0437e99e596d6c2766cccf23de817dce
Andrkln <andrew13338@gmail.com>
2024-05-24 06:33:41 +0300

    trhhrt

--- logic/views.py
+++ logic/views.py
@@ -32,25 +32,20 @@ class ChatBotView(APIView):
                 response_chunks = generate_response(user_question=user_question_text)
 
                 def chunk_generator(response_chunks):
-                    chunks = ''
-                    conversation = Conversation.objects.create()
-                    chat_id_to_send = str(conversation.chat_id)
-                    yield json.dumps({'chat_id': chat_id_to_send}) + '\n'
+                buffer = ''
+                conversation = Conversation.objects.create()
+                chat_id_to_send = str(conversation.chat_id)
+                yield json.dumps({'chat_id': chat_id_to_send}) + '\n'
 
-                    for chunk in response_chunks:
-                        if chunk.choices[0].delta.content:
-                            chunks += chunk.choices[0].delta.content
-                            message = chunk.choices[0].delta.content
-                            yield json.dumps({'message': message, 'id': chunk.id}) + '\n'
+                for chunk in response_chunks:
+                    buffer += chunk.choices[0].delta.content
+                    if buffer.endswith('\n'):
+                        message = buffer.strip()
+                        buffer = ''
+                        yield json.dumps({'message': message, 'id': chunk.id}) + '\n'
 
-                    reply = [
-                        {"role": "assistant", "content": initial_role},
-                        {"role": "assistant", "content": chunks},
-                    ]
-
-                    conversation.user_input = json.dumps(user_question)
-                    conversation.response = json.dumps(reply)
-                    conversation.save()
+                if buffer:
+                    yield json.dumps({'message': buffer.strip()}) + '\n'
 
 
                 streaming_response = StreamingHttpResponse(chunk_generator(response_chunks), content_type='application/json')
